c6138c777386ef2700186925fbd8b02a50ad1ff4
Gustavo Seabra <devguga87@gmail.com>
2025-05-15 15:24:16 -0300

    feat: cria classe abstrata Contribuinte

---
+++ src/entities/Contribuinte.java
@@ -0,0 +1,23 @@
+package entities;
+
+public abstract class Contribuinte {
+    private String nome;
+    protected Double rendaAnual;
+
+    public Contribuinte(){}
+
+    public Contribuinte(String nome, Double rendaAnual){
+        this.nome = nome;
+        this.rendaAnual = rendaAnual;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public abstract Double totalImposto();
+}
